69dfb38387cd210abf13a7dbf158a15ba61896e8
Sayan Sil <sayan227sil@ymail.com>
2022-12-02 19:51:09 +0530

    Fix appbar after flutter update

--- lib/screens/common/appbar.dart
+++ lib/screens/common/appbar.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ecosystem/constants.dart';
 
 AppBar buildAppBar(BuildContext context, String title) {
   return AppBar(
     elevation: 0,
-    brightness: Brightness.dark,
+    systemOverlayStyle:
+        SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
     title: Text(title),
     centerTitle: true,
+    backgroundColor: colorPrimary,
     leading: Builder(
       builder: (context) => IconButton(
         splashColor: Colors.transparent,
