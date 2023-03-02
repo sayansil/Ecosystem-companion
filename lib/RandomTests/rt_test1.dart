3e73dd9ee595269fe8c2fc1c87f0367d294ced7d
Sayan Sil <sayan227sil@ymail.com>
2023-03-02 21:03:47 +0530

    Android build prep

--- lib/RandomTests/rt_test1.dart
+++ lib/RandomTests/rt_test1.dart
@@ -1,9 +1,15 @@
 import 'package:ecosystem/database/database_manager.dart';
 import 'package:ecosystem/database/tableSchema/ecosystem_master.dart';
+import 'package:native_simulator/native_simulator.dart';
 
 void testDb() async {
   List<WorldInstance> rows = await MasterDatabase.instance.readAllRows();
 
   print(rows.length.toString() + " rows fetched.");
   print(rows.last.year.toString() + " is the last year.");
+}
+
+void testFfi(int a, int b) {
+  var sum = add(a, b);
+  print("Sum: " + sum.toString());
 }
