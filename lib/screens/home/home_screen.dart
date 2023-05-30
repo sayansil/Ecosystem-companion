1008e702bd5310e850867981a81a198e7acc8043
sayansil <sayan227sil@ymail.com>
2023-05-30 14:51:43 +0530

    Add firebase configs and optimizations

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,11 +5,13 @@ import 'package:ecosystem/screens/common/navdrawer.dart';
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) => Scaffold(
         resizeToAvoidBottomInset: false,
-        drawer: NavDrawer(currentItem: DrawerItem.home),
+        drawer: const NavDrawer(currentItem: DrawerItem.home),
         appBar: buildNavAppBar(context),
-        body: HomeBody(),
+        body: const HomeBody(),
       );
 }
